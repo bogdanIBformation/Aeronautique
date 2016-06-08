fcd6a55a75f3240f64d282c6379aa53556a630f6
ib <ib@WINIB-ODBPNSE3T.local>
2016-06-08 15:14:22 +0200

    defining the variable "enPlus"

--- Aeronautique/src/aeronautique/Avion.java
+++ Aeronautique/src/aeronautique/Avion.java
@@ -5,6 +5,8 @@ public class Avion {
 	private String nomAv;
 	private int capacite;
 	private String loc;
+	private int enPlus;
+	
 	/**
 	 * Constructeur pour un avion ayant les caracteristiques suivantes:
 	 * @param numAv le numero de l'avion
@@ -18,6 +20,7 @@ public class Avion {
 		this.nomAv = nomAv;
 		this.capacite = capacite;
 		this.loc = loc;
+		this.enPlus = 34;
 	}
 
 	//getters + setters
